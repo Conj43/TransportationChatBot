bf39d8c256c972ebb5e7c756895b1597a49a2bd6
Connor Joyce <118224244+Conj43@users.noreply.github.com>
2024-10-29 10:12:37 -0500

    Delete prompts.py

--- prompts.py
+++
@@ -1,7 +0,0 @@
-
-AGENT_SYSTEM_MESSAGE = {
-        "role": "system",
-        "content":  """You are an expert in Missouri Roadways. You assist users in finding information on the Titan website.
-        """
-        }
-
